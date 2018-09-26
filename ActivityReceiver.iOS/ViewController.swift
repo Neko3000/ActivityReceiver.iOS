5dc5b952c39d17ca9e01abad1969ed4ef8a6d930
Neko3000 <Neko3000@live.com>
2018-09-26 15:43:20 +0900

    init(Project): build basic files

---
+++ ActivityReceiver.iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActivityReceiver.iOS
+//
+//  Created by Xueliang Chen on 9/26/18.
+//  Copyright © 2018 Conceptual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
