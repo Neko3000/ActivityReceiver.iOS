535eb66bd34c383dec245ea4cee0c8144033024c
Neko3000 <Neko3000@live.com>
2018-12-13 13:25:42 +0900

    chore(UIView+draggable): remove UIView+draggable from the project

---
+++ ActivityReceiver.iOS/Controllers/MotionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MotionViewController.swift
+//  ActivityReceiver.iOS
+//
+//  Created by Xueliang Chen on 12/5/18.
+//  Copyright © 2018 Conceptual. All rights reserved.
+//
+
+import UIKit
+
+class MotionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ActivityReceiver.iOS/DataTransferObjects/DeviceAccelerationDTO.swift
@@ -0,0 +1,9 @@
+//
+//  DeviceAcceleration.swift
+//  ActivityReceiver.iOS
+//
+//  Created by Xueliang Chen on 12/5/18.
+//  Copyright © 2018 Conceptual. All rights reserved.
+//
+
+import Foundation
